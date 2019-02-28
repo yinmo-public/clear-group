c3a4a23a59ab9870a5c97e7e9ac79a1abff9008f
yinmo-public <yinmo.contact@gmail.com>
2019-02-28 12:58:27 +0800

    Delete group.py

--- group.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from linepy import *
-yinmo = LINE()
-for inv in yinmo.getGroupIdsInvited():
-    yinmo.rejectGroupInvitation(inv)
-    print("Reject " + yinmo.getGroup(inv).name)
-for groups in yinmo.getGroupIdsJoined():
-    yinmo.leaveGroup(groups)
-    print("Leave " + yinmo.getGroup(gid).name)
