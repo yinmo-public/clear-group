175fa9153c021421205cad0f5d5bd9305d774513
yinmo-public <yinmo.contact@gmail.com>
2019-06-12 11:25:12 +0800

    Update group-en.py

--- group-en.py
+++ group-en.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from linepy import *
+from time import sleep
 yinmo = LINE()
 for inv in yinmo.getGroupIdsInvited():
+    sleep(0.5)
     yinmo.rejectGroupInvitation(inv)
     print("Reject " + yinmo.getGroup(inv).name)
 for groups in yinmo.getGroupIdsJoined():
+    sleep(0.5)
     yinmo.leaveGroup(groups)
     print("Leave " + yinmo.getGroup(groups).name)
     
